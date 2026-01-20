52b2cf697c524221e02dcb6d468273eadd7eca34
muhibudinhanifekinerja-prog <muhibudinhanifekinerja@gmail.com>
2026-01-20 08:53:31 +0700

    Update app-perkembangan.js

--- app-perkembangan.js
+++ app-perkembangan.js
@@ -70,9 +70,40 @@ async function loadFilterKomoditas() {
     console.error('Gagal load komoditas:', err);
   }
 }
+async function loadFilterPasar() {
+  try {
+    const res = await fetch(
+      `${SUPABASE_URL}/rest/v1/pasar?select=id_pasar,nama_pasar,kecamatan&order=nama_pasar.asc`,
+      {
+        headers: {
+          apikey: SUPABASE_KEY,
+          Authorization: `Bearer ${SUPABASE_KEY}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+    const select = document.getElementById('filterPasar');
+
+    // reset
+    select.innerHTML = `<option value="">Semua Pasar</option>`;
+
+    data.forEach(d => {
+      const opt = document.createElement('option');
+      opt.value = d.id_pasar;
+      opt.textContent = `${d.nama_pasar} (${d.kecamatan})`;
+      select.appendChild(opt);
+    });
+
+  } catch (err) {
+    console.error('Gagal load pasar:', err);
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   loadFilterTahun();
   loadFilterKomoditas();
+  loadFilterPasar();
 });
+
 
